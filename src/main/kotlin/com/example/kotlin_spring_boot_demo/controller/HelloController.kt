68bf73a1f010f00ef0583460a397e29b8b30544b
bs01631 <firoz@brainstation-23.com>
2025-11-02 17:55:00 +0600

    new api endpoint added

--- src/main/kotlin/com/example/kotlin_spring_boot_demo/controller/HelloController.kt
+++ src/main/kotlin/com/example/kotlin_spring_boot_demo/controller/HelloController.kt
@@ -2,6 +2,7 @@ package com.example.kotlin_spring_boot_demo.controller
 
 import org.slf4j.LoggerFactory
 import org.springframework.web.bind.annotation.GetMapping
+import org.springframework.web.bind.annotation.PathVariable
 import org.springframework.web.bind.annotation.RestController
 
 @RestController
@@ -14,4 +15,11 @@ class HelloController {
         log.info("Info Log Example")
         return "Hello from Kotlin!"
     }
+
+    @GetMapping("/api/{name}")
+    fun sayHelloByName(@PathVariable name: String): String {
+        log.info("Info Log Example with name: $name")
+        return "Hello, $name"
+    }
+
 }
